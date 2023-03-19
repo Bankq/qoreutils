39a9f407f4bf4aca830b7b53c13eddc85ed6831a
Hang Qian <hangqian90@gmail.com>
2023-03-18 20:51:37 -0700

    [qls] take multiple command line args

--- qls/src/main.rs
+++ qls/src/main.rs
@@ -1,10 +1,18 @@
+use std::env;
 use std::fs;
+use std::path::PathBuf;
 
 fn main() {
-    match fs::read_dir("./") {
-        Ok(res) => res.for_each(|f| {
-            println!("{}", f.unwrap().path().display());
-        }),
-        Err(e) => eprintln!("error {}", e),
-    };
+    let args: Vec<String> = env::args().skip(1).collect();
+    for arg in args.iter() {
+        let p = PathBuf::from(arg);
+        println!("{}:", p.canonicalize().unwrap().display());
+        match fs::read_dir(arg) {
+            Ok(res) => res.for_each(|f| {
+                println!("{}", f.unwrap().file_name().to_str().unwrap());
+            }),
+            Err(e) => eprintln!("error {}", e),
+        };
+        println!();
+    }
 }
