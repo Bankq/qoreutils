9c630a2f7570675c18a20de373686d40a39fba56
Hang Qian <hangqian90@gmail.com>
2023-03-20 12:14:01 -0700

    [qls] command line arguments and flags with Clap
    - support include_dot_files `-a`

--- qls/src/main.rs
+++ qls/src/main.rs
@@ -1,22 +1,51 @@
 use std::env;
 use std::fs;
-use std::path::PathBuf;
+use std::path::Path;
+
+use clap::{Arg, ArgAction, ArgMatches, Command};
+
+#[derive(Debug)]
+struct Config {
+    include_dot_files: bool,
+}
+
+impl Config {
+    pub fn from(options: &ArgMatches) -> Self {
+        Self {
+            include_dot_files: options.get_flag("include_dot_files"),
+        }
+    }
+}
 
 fn main() {
-    let mut args: Vec<String> = env::args().skip(1).collect();
-    if args.is_empty() {
-        args.push(".".to_owned());
-    }
+    let cmd = Command::new("qls")
+        .arg(
+            Arg::new("include_dot_files")
+                .short('a')
+                .action(ArgAction::SetTrue)
+                .help("Do not ingore hidden files (files with names that start with '.'). "),
+        )
+        .arg(Arg::new("paths").action(ArgAction::Append));
 
-    for arg in args.iter() {
-        let p = PathBuf::from(arg);
-        println!("{}:", p.canonicalize().unwrap().display());
-        match fs::read_dir(arg) {
+    let args: Vec<String> = env::args().skip(1).collect();
+    let matches = cmd.get_matches_from(&args);
+    let config = Config::from(&matches);
+    let dirs = matches
+        .get_many::<String>("paths")
+        .map(|v| v.map(Path::new).collect())
+        .unwrap_or_else(|| vec![Path::new(".")]);
+
+    for d in dirs {
+        println!("{}:", d.canonicalize().unwrap().display());
+        match fs::read_dir(d) {
             Ok(res) => res.for_each(|f| {
-                println!("{}", f.unwrap().file_name().to_str().unwrap());
+                let fname = f.unwrap().file_name();
+                let fname = fname.to_str().unwrap();
+                if !fname.starts_with(".") || config.include_dot_files {
+                    println!("{}", fname);
+                }
             }),
             Err(e) => eprintln!("error {}", e),
-        };
-        println!();
+        }
     }
 }
