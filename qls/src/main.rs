3298b5789ba6c8995f53339bebbd58c0488149d0
Hang Qian <hangqian90@gmail.com>
2023-03-20 20:29:35 -0700

    only print section header when multiple input

--- qls/src/main.rs
+++ qls/src/main.rs
@@ -30,21 +30,36 @@ fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let matches = cmd.get_matches_from(&args);
     let config = Config::from(&matches);
+    dbg!(&config);
     let dirs = matches
         .get_many::<String>("paths")
         .map(|v| v.map(Path::new).collect())
         .unwrap_or(vec![Path::new(".")]);
 
+    list(dirs, &config);
+}
+
+fn list(dirs: Vec<&Path>, config: &Config) {
+    let multiple_input = dirs.len() > 1;
     for d in dirs {
-        println!("{}:", d.canonicalize().unwrap().display());
+        if multiple_input {
+            println!("{}:", d.display());
+        }
+
         match fs::read_dir(d) {
-            Ok(res) => res.for_each(|f| {
-                let fname = f.unwrap().file_name();
-                let fname = fname.to_str().unwrap();
-                if !fname.starts_with(".") || config.include_dot_files {
-                    println!("{}", fname);
-                }
-            }),
+            Ok(res) => {
+                res.for_each(|f| {
+                    let fname = f.unwrap().file_name();
+                    let fname = fname.to_str().unwrap();
+                    if !fname.starts_with(".") || config.include_dot_files {
+                        print!("{}\t", fname);
+                    }
+                });
+                println!();
+                if multiple_input {
+                    println!();
+                };
+            }
             Err(e) => eprintln!("error {}", e),
         }
     }
